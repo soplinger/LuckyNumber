54ce7d3ba71dc476baefca03315ec7d59a38b7aa
soplinger <solekibbles4@gmail.com>
2018-10-11 22:25:54 -0400

    im so close to capturing a donkey kong 2 remastered for the pennebacker4 aka pb4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,20 +7,31 @@ public class Main {
         Scanner kbInput1 = new Scanner(System.in);
         System.out.println("Hello, what is your name?: ");
         String userName = kbInput1.nextLine();
-        int userExit = 0;
+        int userExit2 = 0;
+        luckyNumber user = new luckyNumber();
+        double rand1 = user.perfectRandom(0);
 
+        System.out.println("Hello, " + userName + " your lucky number is: " + rand1);
 
-        while(userExit == 0) {
-            luckyNumber user = new luckyNumber();
-            System.out.println("Hello, your lucky number is: " + user.perfectRandom(0));
+        boolean userExit = false;
+        while(userExit == false) {
             System.out.println("To exit press 0, to reprint the name press 1, to enter a new name press 2");
-            userExit = kbInput1.nextInt();
-            if (userExit == 1){
-                System.out.println("Hello, your lucky number is: " + user.perfectRandom(0));
+            userExit2 = kbInput1.nextInt();
+            if (userExit2 == 2){
+                System.out.println("Okay, rebooting...");
+                System.out.println("Hello, what is your name?: ");
+                userName = kbInput1.next();
+                rand1 = user.perfectRandom(0);
+                System.out.println("Hello, " + userName + " your lucky number is: " + rand1);
             }
 
-            else {
-                System.out.println("Okay, rebooting...");
+            else if(userExit2 == 1)  {
+                System.out.println("Hello, " + userName + " your lucky number is: " + rand1);
+            }
+
+            else if (userExit2 == 0){
+                System.out.println("Goodbye, " + userName + "!");
+                userExit = true;
             }
         }
 
